5b96c73bc20acad318aba09b63dad6ee2771ac0b
Erin Anderson <e.anderson1226@gmail.com>
2021-02-11 06:11:02 -0500

    commit tree 38648685b769c12d257b542bf5f07555818b9402

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, "Mr. Chooclate", 65, 'M', "Brown", "wise", true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2, "Rowdy", 30, 'M', "Green", "active", True);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3, "Tabitha", 47, 'F', "Purple", "fast", false);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4, "Sergeant Brown", 82, 'M', "black", "analytical", true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, "Melissa", 1, 'F', "red", "goofy", true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6, "Grinch", 51, 'M', "grey", "peaceful", false);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7, "Wendy", 28, "F", "Pink", "irritable", true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8, null, 29, 'F', "yellow", "short-tempered", null);
